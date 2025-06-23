e528a432afffb6b8a8be8878c42d3234a9ee7f3f
Delta <58563252+delta6626@users.noreply.github.com>
2025-06-23 20:01:51 +0000

    add comments

--- CH01/01_07/01_07-challenge1.c
+++ CH01/01_07/01_07-challenge1.c
@@ -4,13 +4,13 @@ int main()
 {
 	int a, b;
 
-	printf("Type a positive value: ");
+	printf("Type a positive value: "); // Remove string bleed
 	scanf("%d", &b);
 	for (a = 0; a < b; a++)
 	{
 		printf("I must do this %d times\n", b);
 		if (a == 9)
-			break;
+			break; // Put semicolon
 	}
 
 	return (0);
