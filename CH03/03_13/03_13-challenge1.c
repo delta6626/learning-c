d1631ccbd0048dfc0ab049abbd84be5ce44aacf5
Delta <58563252+delta6626@users.noreply.github.com>
2025-06-27 10:20:04 +0000

    03_13 solution

--- CH03/03_13/03_13-challenge1.c
+++ CH03/03_13/03_13-challenge1.c
@@ -2,19 +2,19 @@
 
 int main()
 {
-	int a,b;
+	int a, b;
 
 	printf("Enter two values, separated by a space: ");
-	scanf("%d",&a);
-	scanf("%d",&b);
+	scanf("%d", &a);
+	scanf("%d", &b);
 
-	// add
-	// subtract
-	// multiply
-	// divide
-	// modulus
-	// bit shift right
-	// bit shift left
+	printf("a + b = %d\n", a + b);
+	printf("a-b = %d\n", a - b);
+	printf("a x b = %d\n", a * b);
+	printf("a / b = %.2f\n", (float)a / b);
+	printf("Modulus of a and b = %d\n", a % b);
+	printf("a bit shifted to the right by b = %d\n", a >> b);
+	printf("a bit shifted to the left by b = %d\n", a << b);
 
 	return 0;
 }
