2d2885ff4712e1ce631746d476dfd16085cb1fb5
Delta <58563252+delta6626@users.noreply.github.com>
2025-06-29 08:27:09 +0000

    05_014 solution

--- CH05/05_14/05_14_challenge1.c
+++ CH05/05_14/05_14_challenge1.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 
+void add(int a, int b)
+{
+	int r = a + b;
+	printf("Result of addition is %d\n", r);
+}
+
+void subtract(int a, int b)
+{
+	int r = a - b;
+	printf("Result of subtraction is %d\n", r);
+}
+
+void multiply(int a, int b)
+{
+	int r = a * b;
+	printf("Result of multiplication is %d\n", r);
+}
+
+float divide(int a, int b)
+{
+	float r = a / b;
+	return r;
+}
+
 int main()
 {
-	int a,b;
+	int a, b;
 
 	printf("Enter two values, separated by a space: ");
-	scanf("%d",&a);
-	scanf("%d",&b);
+	scanf("%d", &a);
+	scanf("%d", &b);
 
-	add(a,b);
-	subtract(a,b);
-	multiply(a,b);
-	printf("%d / %d = %f\n",a,b,divide(a,b));
-	
+	add(a, b);
+	subtract(a, b);
+	multiply(a, b);
+	printf("%d / %d = %f\n", a, b, divide(a, b));
+
 	return 0;
 }
