8f251093380b684dfae4ed79d7aaf0b2ed656029
Delta <58563252+delta6626@users.noreply.github.com>
2025-06-29 07:59:58 +0000

    05_09 solution

--- CH05/05_09/05_09-challenge1.c
+++ CH05/05_09/05_09-challenge1.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+void line(int n, char c)
+{
+	for (int i = 0; i < n; i++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
+
 int main()
 {
 	puts("Report on Extraterrestrial Activity");
-	line(35,'-');
+	line(35, '-');
 
-	return(0);
+	return (0);
 }
