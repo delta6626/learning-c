491e2fe1f8ecc13386772101d8a81b316c4dc80d
Delta <58563252+delta6626@users.noreply.github.com>
2025-06-30 06:37:31 +0000

    06_09 solution

--- CH06/06_09/06_09-challenge1.c
+++ CH06/06_09/06_09-challenge1.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+struct Birthday
+{
+	int day;
+	int month;
+	int year;
+};
+
+struct Person
+{
+	struct Birthday birthday;
+	float height;
+	char name[];
+};
 
 int main()
 {
+	struct Person me;
+	me.birthday.day = 13;
+	me.birthday.month = 6;
+	me.birthday.year = 2004;
+	me.height = 0.0017f;
+	strcpy(me.name, "Hasan");
+
+	printf("%s was born on %d/%d/%d and is %f kilometers tall\n", me.name, me.birthday.day, me.birthday.month, me.birthday.year, me.height);
+
 	return 0;
 }
