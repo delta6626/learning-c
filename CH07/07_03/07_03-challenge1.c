5e1fbfc666c9d46f5cb48d956f9b54a04efbb171
Delta <58563252+delta6626@users.noreply.github.com>
2025-07-01 13:59:51 +0000

    07_03 solution

--- CH07/07_03/07_03-challenge1.c
+++ CH07/07_03/07_03-challenge1.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main()
 {
 	char string[] = "Nifty TEXT! 123\n";
 
-	return(0);
+	for (int x = 0; string[x] != '\n'; x++)
+	{
+		if (isalpha(string[x]) && isupper(string[x]))
+		{
+			putchar(tolower(string[x]));
+		}
+		else if (isalpha(string[x]) && islower(string[x]))
+		{
+			putchar(toupper(string[x]));
+		}
+		else
+		{
+			putchar(string[x]);
+		}
+	}
+
+	putchar('\n');
 }
