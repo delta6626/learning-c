4007863cc68a42853618f899b8fe5606886da5ed
Delta <58563252+delta6626@users.noreply.github.com>
2025-07-02 07:32:13 +0000

    07_08 solution

--- CH07/07_08/07_08-challenge1.c
+++ CH07/07_08/07_08-challenge1.c
@@ -2,5 +2,13 @@
 
 int main()
 {
+
+	char itemName[] = "Donuts";
+	int quantity = 10;
+	int price = 3.5;
+	float totalCost = quantity * price;
+
+	printf("You have purchased %d %s at a price of $%d each. The total cost is $%.2f\n.", quantity, itemName, price, totalCost);
+
 	return 0;
 }
