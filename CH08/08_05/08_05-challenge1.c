fa66821e328c532dfabf0aa9f0c8c41528c7bd1c
Delta <58563252+delta6626@users.noreply.github.com>
2025-07-03 19:37:32 +0000

    08_05 solution

--- CH08/08_05/08_05-challenge1.c
+++ CH08/08_05/08_05-challenge1.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
 	char title[] = "Pointers don't intimidate me!\n";
+	char *pt = title;
+	for (int x = 0; x < strlen(title); x++)
+	{
+		printf("Character at address %p is %c\n", pt + x, *(pt + x));
+	}
 
 	return 0;
 }
