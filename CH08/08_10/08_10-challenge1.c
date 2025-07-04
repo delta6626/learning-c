63301f54cffc63c78bae3416baa6e07440554af0
Delta <58563252+delta6626@users.noreply.github.com>
2025-07-04 16:22:51 +0000

    08_10

--- CH08/08_10/08_10-challenge1.c
+++ CH08/08_10/08_10-challenge1.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct m
+{
+	char l;
+	int i;
+};
+
 int main()
 {
+	struct m *a[26];
+	for (int i = 0; i < 26; i++)
+	{
+		a[i] = malloc(sizeof(struct m));
+		if (a[i] == NULL)
+		{
+			printf("Error allocating memory!");
+			exit(0);
+		}
+		a[i]->l = 'A' + i;
+		a[i]->i = i;
+	}
+
+	for (int i = 0; i < 26; i++)
+	{
+		printf("The letter at position %d is %c\n", i, a[i]->l);
+	}
+
+	for (int i = 0; i < 26; i++)
+	{
+		free(a[i]);
+	}
+
 	return 0;
 }
