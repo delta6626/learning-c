26af02d812fefdbee3121545ab5d285ec1159ffd
Delta <58563252+delta6626@users.noreply.github.com>
2025-07-05 19:03:58 +0000

    new changes

--- CH08/08_14/08_14-challenge1.c
+++ CH08/08_14/08_14-challenge1.c
@@ -3,16 +3,39 @@
 
 // write the get_input() function here
 
+char *get_input(size_t size)
+{
+	char *input = malloc(size);
+	if (input == NULL)
+	{
+		printf("Failed to reserve memory");
+		free(input);
+		exit(1);
+	}
+	fgets(input, size, stdin);
+	for (int x = 0; x < size; x++)
+	{
+		if (*(input + x) == '\n')
+		{
+			*(input + x) = '\0';
+			break;
+		}
+	}
+	return input;
+}
+
 int main()
 {
-	char *your_name,*your_city;
+	char *your_name, *your_city;
 
 	printf("Your name: ");
 	your_name = get_input(32);
 	printf("Your city: ");
 	your_city = get_input(32);
 
-	printf("%s lives in %s.\n",your_name,your_city);
+	printf("%s lives in %s.\n", your_name, your_city);
+	free(your_name);
+	free(your_city);
 
 	return 0;
 }
