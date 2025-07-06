cc2de54c1ff867bdfc7dac7f013fc4c68df939b3
Delta <58563252+delta6626@users.noreply.github.com>
2025-07-06 18:08:25 +0000

    09_03 initial solution

--- CH09/09_03/09_03-challenge1.c
+++ CH09/09_03/09_03-challenge1.c
@@ -1,8 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
 	const char filename[] = "hello.txt";
+	char username[50];
+	printf("Enter your name: ");
+	fgets(username, 50, stdin);
+
+	const FILE *fileWriter = fopen(filename, "w");
+	if (fileWriter == NULL)
+	{
+		printf("Error creating file!");
+		exit(1);
+	}
+	else
+	{
+		fputs(username, fileWriter);
+		fclose(fileWriter);
+	}
+
+	for (int i = 0; i < strlen(username); i++)
+	{
+		if (username[i] != '\n')
+		{
+			putchar(username[i]);
+		}
+	}
+	printf(" has written to this file.\n");
+
+	const FILE *fileReader = fopen(filename, "r");
+	char readUsername[50];
+	int x = 0;
+	char c;
+	while (1)
+	{
+		c = fgetc(fileReader);
+		if (c == '\n')
+		{
+			break;
+		}
+		else
+		{
+			readUsername[x] = c;
+			x += 1;
+		}
+	}
+
+	fclose(fileReader);
+
+	printf("The string '%s' was written to the file by %s\n", readUsername, username);
 
 	return 0;
 }
