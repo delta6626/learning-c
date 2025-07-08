9086b6aa5e74f1b776a60c4a77e78c63fc0300d1
Delta <58563252+delta6626@users.noreply.github.com>
2025-07-08 19:54:59 +0000

    09_013 solution

--- CH09/09_13/09_13-challenge1.c
+++ CH09/09_13/09_13-challenge1.c
@@ -4,14 +4,64 @@
 
 void create_file(const char *f)
 {
+	char name[32];
+	FILE *fp = fopen(f, "w");
+	if (fp == NULL)
+	{
+		printf("An error occured while opening the file.\n");
+		exit(-1);
+	}
+	else
+	{
+		printf("Type your name: ");
+		scanf("%s", name);
+		fputs(name, fp);
+		printf("%s has written to the file %s\n", name, f);
+		fclose(fp);
+	}
 }
 
 void read_file(const char *f)
 {
+	FILE *fp = fopen(f, "r");
+	char ch;
+
+	if (fp == NULL)
+	{
+		printf("An error occured while reading the file.\n");
+		exit(-1);
+	}
+
+	while (1)
+	{
+		ch = fgetc(fp);
+		if (ch == EOF)
+		{
+			break;
+		}
+		else
+		{
+			printf("%c", ch);
+		}
+	}
+
+	printf("\n");
+
+	fclose(fp);
 }
 
 void delete_file(const char *f)
 {
+	int status = unlink(f);
+	if (status == 0)
+	{
+		printf("%s has been deleted successfully\n", f);
+	}
+	else
+	{
+		printf("An error occured while deleting the file.\n");
+		exit(-1);
+	}
 }
 
 int main()
