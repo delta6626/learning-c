f11762da170792f43f6762e94988b148243aa7dd
Delta <58563252+delta6626@users.noreply.github.com>
2025-07-10 11:24:43 +0000

    10_06 solution

--- CH10/10_06/10_06-challenge1.c
+++ CH10/10_06/10_06-challenge1.c
@@ -4,16 +4,35 @@
 
 int compare(const void *a, const void *b)
 {
-	return( *(int *)a - *(int *)b);
+	return (*(int *)a - *(int *)b);
 }
 
 int main()
 {
 	// seed the randomizer
+	srand((unsigned int)time(NULL));
 	// allocate storage
+	int grid[5][5];
 	// populate the grid with random values, 0 through 99
+	for (int x = 0; x < 5; x++)
+	{
+		for (int y = 0; y < 5; y++)
+		{
+			grid[x][y] = rand() % 100;
+		}
+	}
 	// quicksort the grid
+	qsort(&grid[0][0], 25, sizeof(int), compare);
+
 	// output the grid
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			printf("%10d", grid[i][j]);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
