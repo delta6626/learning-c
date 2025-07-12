1477616ce2a20736a44a7d7b8c762a2e416d5601
Delta <58563252+delta6626@users.noreply.github.com>
2025-07-12 13:03:08 +0000

    password generator

--- programs/randomPasswordGenerator.c
+++ programs/randomPasswordGenerator.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-void *getPassword(int includeNumbers, int includeSpecialCharacters, char *generatedPassword){
-  
+void *getPassword(char includeNumbers, char includeSpecialCharacters, int length, char *generatedPassword)
+{
+
+  srand((unsigned int)time(NULL));
+
+  const char *characters = "abcdefghijklmnopqrstuvwxyz";
+  const char *numbers = "0123456789";
+  const char *specialCharacters = "*!@#$^&*.,`?><_-";
+
+  char n = '\0';
+  char s = '\0';
+
+  for (int x = 0; x < length; x++)
+  {
+    *(generatedPassword + x) = characters[rand() % strlen(characters)];
+  }
+
+  printf("Your password is: %s\n", generatedPassword);
 };
 
 int main()
 {
 
-  const char *characters = "abcdefghijklmnopqrstuvwxyz";
-  const char *numbers = "0123456789";
-  const char *specialCharacters = "*!@#$^&*.,`?><_-";
   char *generatedPassword;
 
   const int minLength = 8;
   int length;
-  char includeSpecialCharacters = 'y';
-  char includeNumbers = 'y';
+  char includeSpecialCharacters;
+  char includeNumbers;
 
   printf("Enter the length of the required password: ");
   scanf("%d", &length);
@@ -34,27 +50,7 @@ int main()
 
   generatedPassword = malloc(sizeof(char) * length);
 
-  if (includeNumbers == 'y' && includeSpecialCharacters == 'y')
-  {
-    getPassword(1, 1, generatedPassword);
-  }
-  else if (includeNumbers == 'y' && includeSpecialCharacters == 'n')
-  {
-    getPassword(1, 0, generatedPassword);
-  }
-  else if (includeNumbers == 'n' && includeSpecialCharacters == 'y')
-  {
-    getPassword(0, 1, generatedPassword);
-  }
-  else if (includeNumbers == 'n' && includeSpecialCharacters == 'n')
-  {
-    getPassword(0, 0, generatedPassword);
-  }
-  else
-  {
-    printf("Invalid option. Exiting..\n");
-    exit(-1);
-  }
+  getPassword(includeNumbers, includeSpecialCharacters, length, generatedPassword);
 
   return 0;
 }
