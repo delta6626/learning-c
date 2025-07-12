7c4a1ab29dafe73d9d31baeb8ae60e5ba66985a5
Delta <58563252+delta6626@users.noreply.github.com>
2025-07-12 12:27:39 +0000

    random password generator

---
+++ programs/randomPasswordGenerator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+  const char *characters = "abcdefghijklmnopqrstuvwxyz";
+  const char *numbers = "0123456789";
+  const char *specialCharacters = "*!@#$^&*.,`?><_-";
+
+  int maxLength;
+  const int minLength = 8;
+
+  return 0;
+}
