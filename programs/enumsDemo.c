063894b16260397c579ff1937ef1ab6b2be2f9a9
Delta <58563252+delta6626@users.noreply.github.com>
2025-07-13 18:27:57 +0000

    typedef and enum demo

--- programs/enumsDemo.c
+++ programs/enumsDemo.c
@@ -12,5 +12,20 @@ int main()
   enum PowerStatus s1 = ON;
   printf("Power status of your machine : %d\n", s1);
 
+  typedef enum
+  {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+  } Days;
+
+  Days today = SUNDAY;
+
+  printf("Today's index is %d\n", today);
+
   return 0;
 }
